b85019512f2d93476190110f827ee6cb760629d3
Rainer Schulz <info@schulz-solutions.de>
2016-04-04 20:37:35 +0200

    04.04.2016-2

--- dist/js/directives.js
+++ dist/js/directives.js
@@ -2,7 +2,7 @@
  * Created by B026789 on 15.12.2015.
  */
 (function () {
-    'use strict';
+    'use strict'
     angular.module('hw.ng.directives', ['ui-router', 'sf.virtualScroll', 'ui-select', 'ngToast']);
 }());
 /**
